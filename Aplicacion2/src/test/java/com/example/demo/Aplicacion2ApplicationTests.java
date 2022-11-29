6353adf335e67ca9fc75f6ff808484827ec22bb2
mauricioqm04 <mauricioqm04@outlook.com>
2022-11-28 20:51:31 -0500

    Commit inicial con eclipse. Close #1

---
+++ Aplicacion2/src/test/java/com/example/demo/Aplicacion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aplicacion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
