38164658ad23683fe2bfb11885b681038334cfba
mauricioqm04 <mauricioqm04@outlook.com>
2022-11-29 19:57:40 -0500

    Creamos los repositorios user y rol. Close #4

--- Aplicacion2/src/main/java/com/example/demo/controller/UserController.java
+++ Aplicacion2/src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.service.UserService;
 
 @Controller //Se le indica que es un metodo controller
 public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	RoleRepository roleRepository;
 	
 	@GetMapping("/")
 	//El string es para que redireccione a el archivo html en especifico
@@ -14,7 +26,11 @@ public class UserController {
 	
 	@GetMapping("/userForm")
 	//El string es para que redireccione a el archivo html en especifico
-	public String userForm() {
+	public String userForm(Model model) {
+		model.addAttribute("userForm", new User());
+		model.addAttribute("userList", userService.getAllUsers());
+		model.addAttribute("roles",roleRepository.findAll());
+		model.addAttribute("listTab","active");
 		return "user-form/user-view";
 	}
 
